eb33be5a99a66d128851302c1743c177fa21234b
shelupets89 <shelupets89@gmail.com>
2019-02-09 12:47:42 +0200

    Done project

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Dima Shelupets. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
@@ -15,14 +16,30 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
    print("кнопка нажата")
             lightOn = !lightOn
+        
         updateUI()
         
         }
-        
-        
         func updateUI() {
+            let device = AVCaptureDevice.default(for: AVMediaType.video)
+            if let dev = device, dev.hasTorch {
+            view.backgroundColor = .black
+                do
+                {
+            try dev.lockForConfiguration()
+            dev.torchMode = lightOn ? .on : .off
+            dev.unlockForConfiguration()
+        } catch {
+            print(error) }
+        } else {
              view.backgroundColor = lightOn ? .white : .black
+            }
+            
         }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
