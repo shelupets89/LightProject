ab017d630bf7943ecb12aef46f439d11e9efdb03
shelupets89 <shelupets89@gmail.com>
2019-02-09 14:19:04 +0200

    finished

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -12,19 +12,18 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var lampOn: UIImageView!
-    @IBOutlet weak var lampOff: UIImageView!
-    
     var lightOn = true
     
     @IBAction func buttonPressed(_ sender: UIButton) {
    print("кнопка нажата")
             lightOn = !lightOn
         
-        if lightOn {
-            lampOn.isHidden = false
+       if lightOn {
+           lampOn.isHidden = false
         } else {
             lampOn.isHidden = true
         }
+        
         updateUI()
         
         }
